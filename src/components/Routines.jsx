8c342e0018d875179cd2561aaf662b2e1c586cc9
coryla <cory.lance.anderson@gmail.com>
2022-04-13 10:34:55 -0500

    routines page showing activities description etc...

--- src/components/Routines.jsx
+++ src/components/Routines.jsx
@@ -18,9 +18,22 @@ const Routines = () => {
       <h1>ROUTINES</h1>
       {routines.map((routine, idx) => {
         return (
-          <div className="routine_data">
+          <div className="routine_data" key={routine.activity}>
             <h2>Name: {routine.name}</h2>
             <h3>Goal: {routine.goal}</h3>
+            <h5>Creator: {routine.creatorName} </h5>
+            <div className="routine_activities">
+              {routine.activities.map((activity, idx) => (
+                <div key={activity.id}>
+                  <h4>Activity Name: {activity.name}</h4>
+                  <ul>
+                    <li>Description: {activity.description}</li>
+                    <li>Count: {activity.count} </li>
+                    <li>Duration: {activity.duration}</li>
+                  </ul>
+                </div>
+              ))}
+            </div>
           </div>
         );
       })}
